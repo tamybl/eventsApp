9cf8758a605ad07b94343ba2dc8e7e0f76569744
María Paz Thompson <m.paz.thompson@gmail.com>
2018-03-22 20:38:44 -0300

    agregando componente

--- src/Components/EventView/EventView.js
+++ src/Components/EventView/EventView.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { render } from 'react-dom';
+import events from './../Data/events.json';
 
 class Calendar extends Component {
   constructor(){
     super();
   }
 
-
   render(){
+    
     return(
-      
+
     )
 }
 
