5e7f45dd9456fd0d9ca208537a6d173da20676e1
María Paz Thompson <m.paz.thompson@gmail.com>
2018-03-22 19:57:39 -0300

    corrigiendo errores

---
+++ src/Components/EventView/EventView.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import { render } from 'react-dom';
+
+class Calendar extends Component {
+  constructor(){
+    super();
+  }
+
+
+  render(){
+    return(
+      
+    )
+}
+
+}
