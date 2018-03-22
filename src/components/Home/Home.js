45e177ff4c8d090319ff3786d4f2eef5478b6475
tamybl <tamara.bravo.l@gmail.com>
2018-03-22 12:05:14 -0300

    Componentes navbar home y upcomingevents

---
+++ src/components/Home/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+//import propTypes from 'prop-types';
+import { Row, Col, Grid } from 'react-bootstrap';
+import Navbar from './../Navbar/Navbar';
+import Upcomingevents from './../Upcoming/Upcomingevents';
+
+const categories = ['UX', 'Mujeres', 'Programación'];
+
+const menu = {
+  appName: 'Events App',
+  logoImage: 'http://4.bp.blogspot.com/-d3Lc_Bpm1y4/VKidtotyyEI/AAAAAAAAH68/glouCyRYlts/s1600/eventbrite-thumbnail.png',
+  linksmenu1: {
+    title: 'Config',
+    url: 'http://www.google.cl'
+  }
+}
+
+class Home extends Component {
+
+  constructor(props) {
+    super();
+    this.state = categories;
+  }
+
+  render() {
+    let listCategories = this.state.map((name, index) => {
+        return <Col xs={12} sm={6} md={4} key={index} className={`categories ${name}`}  alt={name} onClick={this.getresults}><span className="title">{name}</span></Col>
+      });
+    return (
+      <Grid fluid>
+      <Navbar imageBrand={menu.logoImage} title={menu.appName} linkright1='links1' />
+      <Row className="content">
+        
+        <Upcomingevents />
+      </Row>
+      </Grid>);
+    }
+  }
+
+export default Home;
+      
+
