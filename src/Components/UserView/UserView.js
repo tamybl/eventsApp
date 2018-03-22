c428063c17db54c0c17df371ccf7bf1ef015d0f6
María Paz Thompson <m.paz.thompson@gmail.com>
2018-03-22 10:53:18 -0300

    agregando userview

---
+++ src/Components/UserView/UserView.js
@@ -0,0 +1,20 @@
+import React, {Component} 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+
+class UserView extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <Grid>
+        <div>
+        
+        </div>
+      </Grid>
+    )
+  }
+
+}
